bd6e06faf15466cf13503099ee1cbbb60c2c1ebc
MarcoarCarleti <marcarleti@gmail.com>
2024-02-15 22:59:20 -0300

    feat: add CreateUserController and validations

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import 'dotenv/config.js'
 import express from 'express'
-import { PostgresHelper } from './db/postgres/helper.js'
+import { CreateUserController } from './controllers/create-user.js'
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/api/users', async (req, res) => {
-    const results = await PostgresHelper.query('SELECT * FROM users')
-
-    return res.send(JSON.stringify(results))
-})
-
 app.post('/api/users', async (req, res) => {
-    console.log(req.body)
-    console.log(req.headers)
-    res.statusCode(201).send('User created successfully!')
+    const createUserController = new CreateUserController()
+
+    const { statusCode, body } = await createUserController.execute(req)
+
+    res.status(statusCode).json(body)
 })
 
 app.listen(process.env.PORT, () => {
