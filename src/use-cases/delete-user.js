c396ef3fc6b738cdcab522389a1759dc04363853
MarcoarCarleti <marcarleti@gmail.com>
2024-02-21 18:22:46 -0300

    feat: add dependency injection to delete user

--- src/use-cases/delete-user.js
+++ src/use-cases/delete-user.js
@@ -1,10 +1,11 @@
-import { PostgresDeleteUserRepository } from '../repositories/postgres/index.js'
 
 export class DeleteUserUseCase {
+    constructor(deleteUserRepository) {
+        this.deleteUserRepository = deleteUserRepository
+    }
     async execute(userId) {
-        const deleteUserRepository = new PostgresDeleteUserRepository()
 
-        const deletedUser = await deleteUserRepository.execute(userId)
+        const deletedUser = await this.deleteUserRepository.execute(userId)
 
         return deletedUser
     }
