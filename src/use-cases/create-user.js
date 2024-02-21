a6b4c8eea4c73f79bb8a4ce231908a85e694834f
MarcoarCarleti <marcarleti@gmail.com>
2024-02-21 18:22:38 -0300

    feat: add dependency injection to create user

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,16 +1,20 @@
 import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
-import { PostgresCreateUserRepository,PostgresGetUserByEmailRepository } from '../repositories/postgres/index.js'
 import { EmailAlreadyInUseError } from '../errors/user.js'
 
 export class CreateUserUseCase {
+    constructor(
+        postgresGetUserByEmailRepository,
+        postgresCreateUserRepository,
+    ) {
+        this.postgresGetUserByEmailRepository = postgresGetUserByEmailRepository
+        this.postgresCreateUserRepository = postgresCreateUserRepository
+    }
     async execute(createUserParams) {
         // verificar se o e-mail já está em uso
-        const postgresGetUserByEmailRepository =
-            new PostgresGetUserByEmailRepository()
 
         const userWithProvidedEmail =
-            await postgresGetUserByEmailRepository.execute(
+            await this.postgresGetUserByEmailRepository.execute(
                 createUserParams.email,
             )
 
@@ -32,9 +36,9 @@ export class CreateUserUseCase {
         }
 
         // chamar o repositório
-        const postgresCreateUserRepository = new PostgresCreateUserRepository()
 
-        const createdUser = await postgresCreateUserRepository.execute(user)
+        const createdUser =
+            await this.postgresCreateUserRepository.execute(user)
 
         return createdUser
     }
