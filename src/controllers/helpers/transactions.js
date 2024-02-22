323b47eb05a0fb2824571afe58c832c5ee5a9f7b
MarcoarCarleti <marcarleti@gmail.com>
2024-02-22 18:28:46 -0300

    chore: ensure amount can't be negative

--- src/controllers/helpers/transactions.js
+++ src/controllers/helpers/transactions.js
@@ -3,5 +3,6 @@ import validator from 'validator'
 export const checkIfAmountIsValid = (transaction) =>
     validator.isCurrency(transaction.toString(), {
         digits_after_decimal: [2],
+        allow_negatives: false,
         allow_decimal: false,
     })
