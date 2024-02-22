1b4424c14fa5af136de5ae654157c91cb3f1f5e6
MarcoarCarleti <marcarleti@gmail.com>
2024-02-22 17:30:37 -0300

    refactor: move user controllers to its own folder

--- src/controllers/user/update-user.js
+++ src/controllers/user/update-user.js
@@ -1,4 +1,4 @@
-import { EmailAlreadyInUseError } from '../errors/user.js'
+import { EmailAlreadyInUseError } from '../../errors/user.js'
 import {
     invalidPasswordResponse,
     invalidIdResponse,
@@ -9,10 +9,10 @@ import {
     badRequest,
     ok,
     serverError,
-} from './helpers/index.js'
+} from '../helpers/index.js'
 
 export class UpdateUserController {
-    constructor(updateUserUseCase){
+    constructor(updateUserUseCase) {
         this.updateUserUseCase = updateUserUseCase
     }
 
@@ -61,7 +61,10 @@ export class UpdateUserController {
                 }
             }
 
-            const updatedUser = await this.updateUserUseCase.execute(userId, params)
+            const updatedUser = await this.updateUserUseCase.execute(
+                userId,
+                params,
+            )
 
             return ok(updatedUser)
         } catch (err) {
