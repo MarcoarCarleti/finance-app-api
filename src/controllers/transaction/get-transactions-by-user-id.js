59e840c3804bd4e1cf17e7519e59602ed1449c13
MarcoarCarleti <marcarleti@gmail.com>
2024-03-02 19:31:58 -0300

    feat: add GetTransactionsByUserIdController

---
+++ src/controllers/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,54 @@
+import { EmailAlreadyInUseError } from '../../errors/user.js'
+import { ok } from '../helpers/http.js'
+import {
+    badRequest,
+    checkIfIdIsValid,
+    invalidIdResponse,
+    requiredFieldIsMissingResponse,
+    serverError,
+} from '../helpers/validation.js'
+
+export class GetTransactionsByUserIdController {
+    constructor(getTransactionsByUserIdUseCase) {
+        this.getTransactionsByUserIdUseCase = getTransactionsByUserIdUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.query.userId
+
+            // verificar se o userId foi passado como parâmetro
+
+            if (!userId) {
+                return requiredFieldIsMissingResponse('userId')
+            }
+
+            // verificar se o userId é um ID válido
+
+            const userIdIsValid = checkIfIdIsValid(userId)
+
+            if (!userIdIsValid) {
+                return invalidIdResponse()
+            }
+
+            // chamar o use case
+
+            const transactions =
+                await this.getTransactionsByUserIdUseCase.execute({
+                    userId,
+                })
+
+            // retornar resposta http
+
+            return ok(transactions)
+        } catch (err) {
+            console.log(err)
+
+            if (err instanceof EmailAlreadyInUseError) {
+                return badRequest({ message: err.message })
+            }
+
+            return serverError({ message: err.message })
+        }
+    }
+}
