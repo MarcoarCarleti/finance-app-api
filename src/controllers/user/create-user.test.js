a03a28911878aefb3c60b39e4a755ec0f122e104
MarcoarCarleti <marcarleti@gmail.com>
2024-03-10 18:50:37 -0300

    feat: ensure user is created successfully

---
+++ src/controllers/user/create-user.test.js
@@ -0,0 +1,35 @@
+import { CreateUserController } from '../index.js'
+
+describe('Create User Controller', () => {
+    class CreateUserUseCaseStub {
+        execute(user) {
+            return user
+        }
+    }
+
+    it('should return 200 when creating a user successfully', async () => {
+        // arrange
+        const createUserUseCaseStub = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(
+            createUserUseCaseStub,
+        )
+
+        const httpRequest = {
+            body: {
+                first_name: 'Marco',
+                last_name: 'Carleti',
+                email: 'marcarleti@gmail.com',
+                password: '1234567',
+            },
+        }
+
+        // act
+
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+
+        expect(result.statusCode).toBe(200)
+        expect(result.body).toBeTruthy()
+    })
+})
