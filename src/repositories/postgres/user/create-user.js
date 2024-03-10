637ccfd31beded9aeab02b35f3636f8d072c3c0f
MarcoarCarleti <marcarleti@gmail.com>
2024-03-10 15:20:02 -0300

    feat: ensure CreateUser and GetUserByEmailRepository use Prisma

--- src/repositories/postgres/user/create-user.js
+++ src/repositories/postgres/user/create-user.js
@@ -1,23 +1,17 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresCreateUserRepository {
     async execute(createUserParams) {
-        await PostgresHelper.query(
-            'INSERT INTO users (ID, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)',
-            [
-                createUserParams.ID,
-                createUserParams.first_name,
-                createUserParams.last_name,
-                createUserParams.email,
-                createUserParams.password,
-            ],
-        )
+        const user = await prisma.user.create({
+            data: {
+                id: createUserParams.ID,
+                email: createUserParams.email,
+                first_name: createUserParams.first_name,
+                last_name: createUserParams.last_name,
+                password: createUserParams.password,
+            },
+        })
 
-        const createdUser = await PostgresHelper.query(
-            'SELECT * FROM users WHERE ID = $1',
-            [createUserParams.ID],
-        )
-
-        return createdUser[0]
+        return user
     }
 }
