381a85d1b3a6c6f075506236eb13ac7e1e041db0
MarcoarCarleti <marcarleti@gmail.com>
2024-03-10 18:57:16 -0300

    feat: ensure  400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -32,4 +32,30 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(200)
         expect(result.body).toBeTruthy()
     })
+
+    it('should return 400 if first_name is not provided', async () => {
+        // arrange
+
+        const createUserUseCaseStub = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(
+            createUserUseCaseStub,
+        )
+
+        const httpRequest = {
+            body: {
+                last_name: 'Carleti',
+                email: 'marcarleti@gmail.com',
+                password: '1234567',
+            },
+        }
+
+        // act
+
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+
+        expect(result.statusCode).toBe(400)
+        expect(result.body.first_name).toBeUndefined()
+    })
 })
